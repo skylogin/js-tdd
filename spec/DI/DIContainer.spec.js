9f04c65af1b0ca73538c6c04f5591fe874d6c1fd
Chanho Jung <skylogin@gmail.com>
2017-03-11 17:27:25 +0900

    back to tab, tab is best indent system

--- spec/DI/DIContainer.spec.js
+++ spec/DI/DIContainer.spec.js
@@ -1,59 +1,59 @@
 describe('DIContainer', () => {
-  var container;
-  beforeEach(() => {
-    container = new DIContainer();
-  });
+	var container;
+	beforeEach(() => {
+		container = new DIContainer();
+	});
 
-  describe('register(name, dependencies, func)', () => {
-    it('인자가 하나라도 빠졌거나 타입이 잘못되면 예외를 던진다.', () => {
-      let badArgs = [
-        [],   //인자가 없는경우
-        ['Name'],   //name만 있는 경우
-        ['Name', ['Dependency1', 'Dependency2']],   //name과 dependencies만 있는 경우
-        ['Name', () => {}],   //dependencies가 빠진경우
-        // ['Name', (function() {})],   //dependencies가 빠진경우
-        //그 외 경우 모음
-        [1, ['a', 'b'], () => {}],
-        // [1, ['a', 'b'], function() {}],
-        ['Name', [1,2], () => {}],
-        // ['Name', [1,2], function() {}],
-        ['Name', ['a', 'b'], 'should be a function']
-      ];
+	describe('register(name, dependencies, func)', () => {
+		it('인자가 하나라도 빠졌거나 타입이 잘못되면 예외를 던진다.', () => {
+			let badArgs = [
+				[],   //인자가 없는경우
+				['Name'],   //name만 있는 경우
+				['Name', ['Dependency1', 'Dependency2']],   //name과 dependencies만 있는 경우
+				['Name', () => {}],   //dependencies가 빠진경우
+				// ['Name', (function() {})],   //dependencies가 빠진경우
+				//그 외 경우 모음
+				[1, ['a', 'b'], () => {}],
+				// [1, ['a', 'b'], function() {}],
+				['Name', [1,2], () => {}],
+				// ['Name', [1,2], function() {}],
+				['Name', ['a', 'b'], 'should be a function']
+			];
 
-      badArgs.forEach((args) => {
-        expect(() => {
-          container.register.apply(container, args);
-        // }).toThrow();
-        }).toThrowError(container.messages.registerRequiresArgs);   //더 정확한 테스트 (에러메시지까지 본다)
-        // }).toThrowError(container.registerRequiresArgs);   //더 정확한 테스트 (에러메시지까지 본다)
-      });
-    });
-  });
+			badArgs.forEach((args) => {
+				expect(() => {
+					container.register.apply(container, args);
+				// }).toThrow();
+				}).toThrowError(container.messages.registerRequiresArgs);   //더 정확한 테스트 (에러메시지까지 본다)
+				// }).toThrowError(container.registerRequiresArgs);   //더 정확한 테스트 (에러메시지까지 본다)
+			});
+		});
+	});
 
 });
 
 DIContainer = function(){
-  if(!(this instanceof DIContainer)){
-    return new DIContainer();
-  }
+	if(!(this instanceof DIContainer)){
+		return new DIContainer();
+	}
 };
 
 DIContainer.prototype.messages = {
-  registerRequiresArgs: '이 생성자 함수는 인자가 3개 있어야 합니다: 문자열, 문자열 배열, 함수'
+	registerRequiresArgs: '이 생성자 함수는 인자가 3개 있어야 합니다: 문자열, 문자열 배열, 함수'
 };
 
 DIContainer.prototype.register = function(name, dependencies, func){
-  let ix;
+	let ix;
 
-  if(typeof name !== 'string' || !Array.isArray(dependencies) || typeof func !== 'function'){
-    throw new Error(this.messages.registerRequiresArgs);
-  }
+	if(typeof name !== 'string' || !Array.isArray(dependencies) || typeof func !== 'function'){
+		throw new Error(this.messages.registerRequiresArgs);
+	}
 
-  for(ix=0; ix<dependencies.length; ++ix){
-    if(typeof dependencies[ix] !== 'string'){
-      throw new Error(this.messages.registerRequiresArgs);
-    }
-  }
+	for(ix=0; ix<dependencies.length; ++ix){
+		if(typeof dependencies[ix] !== 'string'){
+			throw new Error(this.messages.registerRequiresArgs);
+		}
+	}
 };
 
 
--- spec/DI/DIContainer3.spec.js
+++ spec/DI/DIContainer3.spec.js
@@ -1,119 +1,119 @@
 describe('DIContainer', () => {
-  var container;
-  beforeEach(() => {
-    container = new DIContainer();
-  });
+	var container;
+	beforeEach(() => {
+		container = new DIContainer();
+	});
 
-  describe('register(name, dependencies, func)', () => {
-    it('인자가 하나라도 빠졌거나 타입이 잘못되면 예외를 던진다.', () => {
-      let badArgs = [
-        [],   //인자가 없는경우
-        ['Name'],   //name만 있는 경우
-        ['Name', ['Dependency1', 'Dependency2']],   //name과 dependencies만 있는 경우
-        ['Name', () => {}],   //dependencies가 빠진경우
-        //그 외 경우 모음
-        [1, ['a', 'b'], () => {}],
-        ['Name', [1,2], () => {}],
-        ['Name', ['a', 'b'], 'should be a function']
-      ];
+	describe('register(name, dependencies, func)', () => {
+		it('인자가 하나라도 빠졌거나 타입이 잘못되면 예외를 던진다.', () => {
+			let badArgs = [
+				[],   //인자가 없는경우
+				['Name'],   //name만 있는 경우
+				['Name', ['Dependency1', 'Dependency2']],   //name과 dependencies만 있는 경우
+				['Name', () => {}],   //dependencies가 빠진경우
+				//그 외 경우 모음
+				[1, ['a', 'b'], () => {}],
+				['Name', [1,2], () => {}],
+				['Name', ['a', 'b'], 'should be a function']
+			];
 
-      badArgs.forEach((args) => {
-        expect(() => {
-          container.register.apply(container, args);
-        }).toThrowError(container.registerRequiresArgs);    //더 정확한 테스트 (에러메시지까지 본다)
-      });
-    });
-  });
+			badArgs.forEach((args) => {
+				expect(() => {
+					container.register.apply(container, args);
+				}).toThrowError(container.registerRequiresArgs);    //더 정확한 테스트 (에러메시지까지 본다)
+			});
+		});
+	});
 
-  describe('get(name)', () => {
-    it('성명이 등록되어 있지 않으면 undefined를 반환한다', () => {
-      expect(container.get('notDefined')).toBeUndefined();
-    });
+	describe('get(name)', () => {
+		it('성명이 등록되어 있지 않으면 undefined를 반환한다', () => {
+			expect(container.get('notDefined')).toBeUndefined();
+		});
 
-    it('등록된 함수를 실행한 결과를 반환한다', () => {
-      let name = 'MyName';
-      let returnFromRegisteredFunction = 'something';
+		it('등록된 함수를 실행한 결과를 반환한다', () => {
+			let name = 'MyName';
+			let returnFromRegisteredFunction = 'something';
 
-      container.register(name, [], () => {
-        return returnFromRegisteredFunction;
-      });
+			container.register(name, [], () => {
+				return returnFromRegisteredFunction;
+			});
 
-      expect(container.get(name)).toBe(returnFromRegisteredFunction);
-    });
+			expect(container.get(name)).toBe(returnFromRegisteredFunction);
+		});
 
-    it('등록된 함수에 의존성을 제공한다', () => {
-      let main = 'main';
-      let mainFunc;
-      let dep1 = 'dep1';
-      let dep2 = 'dep2';
+		it('등록된 함수에 의존성을 제공한다', () => {
+			let main = 'main';
+			let mainFunc;
+			let dep1 = 'dep1';
+			let dep2 = 'dep2';
 
-      container.register(main, [dep1, dep2], (dep1Func, dep2Func) => {
-        return (() => {
-          return dep1Func() + dep2Func();
-        });
-      });
+			container.register(main, [dep1, dep2], (dep1Func, dep2Func) => {
+				return (() => {
+					return dep1Func() + dep2Func();
+				});
+			});
 
-      container.register(dep1, [], () => {
-        return (() => {
-          return 1;
-        });
-      });
+			container.register(dep1, [], () => {
+				return (() => {
+					return 1;
+				});
+			});
 
-      container.register(dep2, [], () => {
-        return (() => {
-          return 2;
-        });
-      });
+			container.register(dep2, [], () => {
+				return (() => {
+					return 2;
+				});
+			});
 
-      mainFunc = container.get(main);
-      expect(mainFunc()).toBe(3);
-    });
-  });
+			mainFunc = container.get(main);
+			expect(mainFunc()).toBe(3);
+		});
+	});
 
 });
 
 
 class DIContainer{
-  constructor(){
-    this.registerRequiresArgs = '이 생성자 함수는 인자가 3개 있어야 합니다: 문자열, 문자열 배열, 함수';
-    this.registrations = [];
+	constructor(){
+		this.registerRequiresArgs = '이 생성자 함수는 인자가 3개 있어야 합니다: 문자열, 문자열 배열, 함수';
+		this.registrations = [];
 
-    if(!(this instanceof DIContainer)){
-      return new DIContainer();
-    }
-  }
+		if(!(this instanceof DIContainer)){
+			return new DIContainer();
+		}
+	}
 
-  register(name, dependencies, func){
-    if(typeof name !== 'string' || !Array.isArray(dependencies) || typeof func !== 'function'){
-      throw new Error(this.registerRequiresArgs);
-    }
+	register(name, dependencies, func){
+		if(typeof name !== 'string' || !Array.isArray(dependencies) || typeof func !== 'function'){
+			throw new Error(this.registerRequiresArgs);
+		}
 
-    for(let ix=0; ix<dependencies.length; ++ix){
-      if(typeof dependencies[ix] !== 'string'){
-        throw new Error(this.registerRequiresArgs);
-      }
-    }
+		for(let ix=0; ix<dependencies.length; ++ix){
+			if(typeof dependencies[ix] !== 'string'){
+				throw new Error(this.registerRequiresArgs);
+			}
+		}
 
-    this.registrations[name] = {
-      dependencies: dependencies,
-      func: func
-    };
-  }
+		this.registrations[name] = {
+			dependencies: dependencies,
+			func: func
+		};
+	}
 
-  get(name){
-    const self = this;
-    const registration = this.registrations[name];
-    const dependencies = [];
+	get(name){
+		const self = this;
+		const registration = this.registrations[name];
+		const dependencies = [];
 
-    if(registration === undefined){
-      return undefined;
-    }
+		if(registration === undefined){
+			return undefined;
+		}
 
-    registration.dependencies.forEach((dependencyName) => {
-      let dependency = self.get(dependencyName);
-      dependencies.push( (dependency === undefined)? undefind: dependency);
-    });
+		registration.dependencies.forEach((dependencyName) => {
+			let dependency = self.get(dependencyName);
+			dependencies.push( (dependency === undefined)? undefind: dependency);
+		});
 
-    return registration.func.apply(undefined, dependencies);
-  }
+		return registration.func.apply(undefined, dependencies);
+	}
 }
